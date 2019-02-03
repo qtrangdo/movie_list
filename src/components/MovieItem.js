386e51ddacbc670c9b925a8a879f064c3a8096de
td94 <quynhtrangdo94@gmail.com>
2019-02-03 09:19:13 -0800

    Add watch/to watch buttons

--- src/components/MovieItem.js
+++ src/components/MovieItem.js
@@ -1,16 +1,35 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 
-class MovieItem extends Component{
+class MovieItem extends Component {
     constructor(props) {
-        super(props)
+        super(props);
+        this.state = {
+            watched: true
+        }
+    }
+
+    onClick() {
+        let watched = !this.state.watched
+        this.setState({ watched })
     }
 
     render() {
         return (
             <div className='card-body border'>
                 <h5 className='card-title'>
-                    {this.props.movie.title}
+                    <div className='row'>
+                        <div className="col col-sm-8">
+                            {this.props.movie.title}
+                        </div>
+                        <div className="col col-sm-4">
+                        {this.state.watched
+                            ? <button className="btn btn-success btn-sm" onClick={this.onClick.bind(this)}> Watched</button>
+                            : <button className="btn btn-info btn-sm" onClick={this.onClick.bind(this)}> To watched</button>
+                        }
+                            
+                        </div>
+                    </div>
                 </h5>
             </div>
         )
